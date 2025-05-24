07e74059b03312495182b2149e19d24e7f715f03
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-05-23 22:53:04 -0300

    FnBot-MD

--- plugins/4.js
+++ plugins/4.js
@@ -1,28 +1,20 @@
 const initHandler = async (m, { conn, usedPrefix }) => {
     const buttons = [
-        {
-            buttonId: `${usedPrefix}owner`,
-            buttonText: { displayText: "👑 Owner" },
-            type: 1,
-        },
-        {
-            buttonId: `${usedPrefix}ping`,
-            buttonText: { displayText: "🏓 Ping" },
-            type: 1,
-        },
-    ];
+        { buttonId: `${usedPrefix}.owner`, buttonText: { displayText: "👑 Owner" }, type: 1 },
+        { buttonId: `${usedPrefix}.ping`, buttonText: { displayText: "🏓 Ping" }, type: 1 },
+    ]
 
     await conn.sendMessage(
         m.chat,
         {
             text: "🔹 Selecciona una opción:",
-            buttons: buttons,
-            viewOnce: true,
+            buttons,
+            headerType: 1 // Importante para que los botones funcionen
         },
         { quoted: m }
-    );
-};
+    )
+}
 
-initHandler.command = /^init$/i;
+initHandler.command = /^init$/i
 
-export default initHandler;
+export default initHandler
