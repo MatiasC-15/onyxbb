4551af5317003300dfe2370571a8cbc69dc1e8dc
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-05-24 23:13:42 -0300

    FnBot-MD

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,19 +1,14 @@
-
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) { 
-    let numcreador = '584146277368';
+    let numcreador = '593979133620';
     let ownerJid = numcreador + '@s.whatsapp.net';
 
     let name = await conn.getName(ownerJid) || 'Owner'; 
-    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y del Bot Barboza Ai';
-    let empresa = 'Barboza- Servicios Tecnológicos';
-    let imagen = 'https://files.catbox.moe/l81ahk.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
 
-    // Enviar imagen junto con el número del dueño y sus detalles
     await conn.sendMessage(m.chat, { 
-        image: { url: imagen },
-        caption: `👤 *Dueño del bot*\n📌 *Nombre:* ${name}\n📞 *Número:* wa.me/${numcreador}\n📝 *Descripción:* ${about}\n🏢 *Empresa:* ${empresa}\n📧 *Email:* sebastianbarbaro82@gmail.com\n🌐 *Instagram:* https://www.instagram.com/sebastian_barboza13`,
+        text: `👤 *Owner*\n📌 *Nombre:* ${name}\n📝 *Descripción:* ${about}`
     }, { quoted: m });
 }
 
