2b49eadf2b0790512e90ac17be4dd914bebed23b
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-05-27 17:57:51 -0300

    Actualizar descargas-ff.js

--- plugins/descargas-ff.js
+++ plugins/descargas-ff.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command }) => {
   try {
     let res = await fetch('https://api.dorratz.com/v2/pix-ai?prompt=anime+alya')
     let json = await res.json()
-    if (!json.status || !json.result) throw 'Sin resultado.'
+    if (!json.status || !json.result) throw '❌ Sin resultado.'
     await conn.sendFile(m.chat, json.result, 'alya.jpg', '✨', m)
   } catch {
     await m.reply('❌ Error, intenta de nuevo más tarde.')
