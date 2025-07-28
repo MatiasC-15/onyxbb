730ac72c69f7bd3d4424928b863db021681c447c
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-07-27 23:40:54 -0300

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,13 +3,27 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
-  let img = await (await fetch(`${pp}`)).buffer()
+  
+  const fkontak = { 
+    "key": { 
+      "participants": "0@s.whatsapp.net", 
+      "remoteJid": "status@broadcast", 
+      "fromMe": false, 
+      "id": "Halo" 
+    }, 
+    "message": { 
+      "contactMessage": { 
+        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+      }
+    }, 
+    "participant": "0@s.whatsapp.net"
+  }  
+  
   let chat = global.db.data.chats[m.chat]
   let txt = 'ゲ◜៹ New Member ៹◞ゲ'
   let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
   let groupSize = participants.length
+  
   if (m.messageStubType == 27) {
     groupSize++;
   } else if (m.messageStubType == 28 || m.messageStubType == 32) {
@@ -18,10 +32,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   if (chat.welcome && m.messageStubType == 27) {
     let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
+    await conn.sendMini(m.chat, txt, dev, bienvenida, null, null, null, fkontak)
   }
-  
+
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
     let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
-  }}
+    await conn.sendMini(m.chat, txt1, dev, bye, null, null, null, fkontak)
+  }
+}
